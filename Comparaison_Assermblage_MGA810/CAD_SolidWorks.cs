c17d96fef1eaed32d185fbcb16bce2d187875655
Maxime Royal <MaxRoyal123@hotmail.com>
2021-11-08 23:40:36 -0500

    Implémentation de méthode bidons dans la classe CAD_SolidWorks

--- Comparaison_Assermblage_MGA810/CAD_SolidWorks.cs
+++ Comparaison_Assermblage_MGA810/CAD_SolidWorks.cs
@@ -17,12 +17,93 @@ namespace Comparaison_Assemblage_MGA810
 
         public override void CloseModel(Model model)
         {
-            
+
         }
 
-        public override List<Model> GetComponents(Model model) 
+
+        public override List<Model> GetComponents(Model model)
         {
             return new List<Model>();
-        }              
+        }
+
+
+        public override List<Model> GetConfigurations(Model model)
+        {
+            return new List<Model>();
+        }
+
+
+        public override string GetMaterial(Model model)
+        {
+            return "";
+        }
+
+        public override double GetMass(Model model)
+        {
+            return 0.0;
+        }
+
+        public override double GetVolume(Model model)
+        {
+            return 0.0;
+        }
+
+        public override double GetSurfaceArea(Model model)
+        {
+            return 0.0;
+        }
+
+        public override int GetNbFaces(Model model)
+        {
+            return 0;
+        }
+
+        public override int GetNbEdges(Model model)
+        {
+            return 0;
+        }
+
+        public override string GetColors(Model model)
+        {
+            return "";
+        }
+
+
+        public override System.Numerics.Vector3 GetCenterOfMass(Model model)
+        {
+            return new System.Numerics.Vector3();
+        }
+
+        public override System.Numerics.Matrix4x4 GetPrincipalAxes(Model model)
+        {
+            return new System.Numerics.Matrix4x4();
+        }
+
+        public override System.Numerics.Vector3 GetInertia(Model model)
+        {
+            return new System.Numerics.Vector3();
+        }
+
+        public override System.Numerics.Matrix4x4 GetInertiaTensorAtCM(Model model)
+        {
+            return new System.Numerics.Matrix4x4();
+        }
+
+        public override System.Numerics.Matrix4x4 GetInertiaTensorAtFOR(Model model)
+        {
+            return new System.Numerics.Matrix4x4();
+        }
+
+
+        public override List<object> GetConstraints(Model model)
+        {
+            return new List<object>();
+        }
+
+
+        public override KeyValuePair<string, string> GetCustomProperties(Model model)
+        {
+            return new KeyValuePair<string, string>();
+        }
     }
 }
