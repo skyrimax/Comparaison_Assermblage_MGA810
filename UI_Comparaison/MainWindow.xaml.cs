725e9f90b93b9ad6132a3269d978a28b709f74d2
PhilEtsProg <77866468+PhilEtsProg@users.noreply.github.com>
2021-11-15 21:37:52 -0500

    Ajout d'un deuxième projet exclusivement dédié à l'UI.

---
+++ UI_Comparaison/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace UI_Comparaison
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // public string Directory   { get;set value =
+        // OnPropertyChanged("Directory");
+ //   }
+
+
+    public MainWindow()
+        {
+            InitializeComponent();
+                        
+
+        }
+
+        // Référence vers les deux assemblages (2 objects de la classe assembly.cs)
+        // Référence vers deux objets de CAD_Software (Si même software Driver 1 = Driver 2)
+
+    }
+}
