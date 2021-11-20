38825c14541693071bb5bf98f0f19e8b9f288d9d
PhilEtsProg <77866468+PhilEtsProg@users.noreply.github.com>
2021-11-19 20:59:57 -0500

    Ajout de la classe ElementsToCompare

---
+++ Comparaison_Assermblage_MGA810/Models/ElementsToCompare.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comparaison_Assemblage_MGA810.Models
+{
+    class ElementsToCompare
+    {
+    }
+}
