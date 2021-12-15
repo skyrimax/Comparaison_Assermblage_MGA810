72a11ff5db9e1c87745401c1530de2966c3cf1aa
Julien Voyer <julien.voyer.1@ens.etsmtl.ca>
2021-12-15 03:36:45 -0500

    Inertia functions implemented

--- Comparaison_Assermblage_MGA810/CAD_SolidWorks.cs
+++ Comparaison_Assermblage_MGA810/CAD_SolidWorks.cs
@@ -80,9 +80,9 @@ namespace Comparaison_Assemblage_MGA810
 
         protected override System.DateTime GetSaveDateTime(Model model)
         {
-            var SolidworksSaveTime = ((SldWorks.ModelDoc2)(((IModel)model).RefToComponent)).get_SummaryInfo((int)swSummInfoField_e.swSumInfoSaveDate);
-            var AssemblySaveTime = DateTime.Parse(SolidworksSaveTime);
-            return AssemblySaveTime;
+            var solidworksSaveTime = ((SldWorks.ModelDoc2)(((IModel)model).RefToComponent)).get_SummaryInfo((int)swSummInfoField_e.swSumInfoSaveDate);
+            var assemblySaveTime = DateTime.Parse(solidworksSaveTime);
+            return assemblySaveTime;
         }
 
 
@@ -146,7 +146,9 @@ namespace Comparaison_Assemblage_MGA810
 
         protected override string GetActiveConfiguration(Model model)
         {
-            return "";
+            string activeConfiguration;
+            activeConfiguration = swApp.GetActiveConfigurationName(((IModel)model).Path);
+            return activeConfiguration;
         }
 
 
@@ -162,7 +164,9 @@ namespace Comparaison_Assemblage_MGA810
 
         protected override double GetMass(Model model)
         {
-            return 0.0;
+            MassProperty2 allMassInfo = CreateMassProperty2FromModel(model);
+            double modelMass = allMassInfo.Mass;
+            return modelMass;
         }
 
         protected override double GetVolume(Model model)
@@ -171,7 +175,7 @@ namespace Comparaison_Assemblage_MGA810
             {
                 throw new ArgumentException("Parameter is not an assembly", nameof(model));
             }
-
+            // Using a density of 0 to simply extract volume
             return ((SldWorks.Body2)(((SldWorks.PartDoc)((IModel)model).RefToComponent).GetBodies2((int)swBodyType_e.swSolidBody, true)[0])).GetMassProperties(0)[3];
         }
 
@@ -223,22 +227,88 @@ namespace Comparaison_Assemblage_MGA810
 
         protected override System.Numerics.Vector3 GetCenterOfMass(Model model)
         {
-            return new System.Numerics.Vector3();
-        }
 
+            MassProperty2 allMassInfo = CreateMassProperty2FromModel(model);
+            double[] modelCOM= allMassInfo.CenterOfMass;
+            System.Numerics.Vector3 CtrOfMass;
+
+            CtrOfMass.X = (float)modelCOM[0];
+            CtrOfMass.Y = (float)modelCOM[1];
+            CtrOfMass.Z = (float)modelCOM[2];
+
+            return CtrOfMass;
+        }
+        
         protected override System.Numerics.Matrix4x4 GetPrincipalAxes(Model model)
         {
-            return new System.Numerics.Matrix4x4();
+            MassProperty2 allMassInfo = CreateMassProperty2FromModel(model);
+            allMassInfo.Recalculate();
+            double[] modelPrincipalAxesX = allMassInfo.PrincipalAxesOfInertia[0];
+            double[] modelPrincipalAxesY = allMassInfo.PrincipalAxesOfInertia[1];
+            double[] modelPrincipalAxesZ = allMassInfo.PrincipalAxesOfInertia[2];
+            System.Numerics.Matrix4x4 principalAxis_Matrix;
+
+            principalAxis_Matrix.M11 = (float)modelPrincipalAxesX[0];
+            principalAxis_Matrix.M12 = (float)modelPrincipalAxesX[1];
+            principalAxis_Matrix.M13 = (float)modelPrincipalAxesX[2];
+            principalAxis_Matrix.M14 = 0;
+            principalAxis_Matrix.M21 = (float)modelPrincipalAxesY[0];
+            principalAxis_Matrix.M22 = (float)modelPrincipalAxesY[1];
+            principalAxis_Matrix.M23 = (float)modelPrincipalAxesY[2];
+            principalAxis_Matrix.M24 = 0;
+            principalAxis_Matrix.M31 = (float)modelPrincipalAxesZ[0];
+            principalAxis_Matrix.M32 = (float)modelPrincipalAxesZ[1];
+            principalAxis_Matrix.M33 = (float)modelPrincipalAxesZ[2];
+            principalAxis_Matrix.M34 = 0;
+            principalAxis_Matrix.M41 = 0;
+            principalAxis_Matrix.M42 = 0;
+            principalAxis_Matrix.M43 = 0;
+            principalAxis_Matrix.M44 = 1;
+
+            return principalAxis_Matrix;
+            
         }
 
         protected override System.Numerics.Vector3 GetInertia(Model model)
         {
-            return new System.Numerics.Vector3();
+            MassProperty2 allMassInfo = CreateMassProperty2FromModel(model);
+            allMassInfo.Recalculate();
+            var PrincipalMomentsofInertiaReturnArray = allMassInfo.PrincipalMomentsOfInertia;
+            System.Numerics.Vector3 PrincipalMoments;
+            PrincipalMoments.X = (float)PrincipalMomentsofInertiaReturnArray[0];
+            PrincipalMoments.Y = (float)PrincipalMomentsofInertiaReturnArray[1];
+            PrincipalMoments.Z = (float)PrincipalMomentsofInertiaReturnArray[2];
+
+            return PrincipalMoments;
+
         }
 
         protected override System.Numerics.Matrix4x4 GetInertiaTensorAtCM(Model model)
         {
-            return new System.Numerics.Matrix4x4();
+            MassProperty2 allMassInfo = CreateMassProperty2FromModel(model);
+            allMassInfo.Recalculate();
+            double[] InertiaAroundCOM = allMassInfo.GetMomentOfInertia(0);
+         
+            System.Numerics.Matrix4x4 InertiaAroundCOM_Matrix;
+
+            InertiaAroundCOM_Matrix.M11 = (float)InertiaAroundCOM[0];
+            InertiaAroundCOM_Matrix.M12 = (float)InertiaAroundCOM[1];
+            InertiaAroundCOM_Matrix.M13 = (float)InertiaAroundCOM[2];
+            InertiaAroundCOM_Matrix.M14 = 0;
+            InertiaAroundCOM_Matrix.M21 = (float)InertiaAroundCOM[3];
+            InertiaAroundCOM_Matrix.M22 = (float)InertiaAroundCOM[4];
+            InertiaAroundCOM_Matrix.M23 = (float)InertiaAroundCOM[5];
+            InertiaAroundCOM_Matrix.M24 = 0;
+            InertiaAroundCOM_Matrix.M31 = (float)InertiaAroundCOM[6];
+            InertiaAroundCOM_Matrix.M32 = (float)InertiaAroundCOM[7];
+            InertiaAroundCOM_Matrix.M33 = (float)InertiaAroundCOM[8];
+            InertiaAroundCOM_Matrix.M34 = 0;
+            InertiaAroundCOM_Matrix.M41 = 0;
+            InertiaAroundCOM_Matrix.M42 = 0;
+            InertiaAroundCOM_Matrix.M43 = 0;
+            InertiaAroundCOM_Matrix.M44 = 1;
+
+            return InertiaAroundCOM_Matrix;
         }
 
         protected override System.Numerics.Matrix4x4 GetInertiaTensorAtFOR(Model model)
@@ -252,10 +322,17 @@ namespace Comparaison_Assemblage_MGA810
             return new List<object>();
         }
 
-
         protected override KeyValuePair<string, string> GetCustomProperties(Model model)
         {
             return new KeyValuePair<string, string>();
+        }
+        private MassProperty2 CreateMassProperty2FromModel(Model model)
+        {
+
+            ModelDocExtension massExtension = ((ModelDoc2)((SldWorks.PartDoc)((IModel)model).RefToComponent)).Extension;
+            massExtension.SelectByID2("", "BODYFEATURE", 0, 0, 0, true, 1, null, 1);
+            MassProperty2 allMassInfo = (MassProperty2)massExtension.CreateMassProperty2();
+            return allMassInfo;
         }
 
         private readonly SldWorks.SldWorks swApp;
